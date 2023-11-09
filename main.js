1f5a625f55cf89943368d7042bbb4c7114507faa
juan Ignacio Hussen <juanihussen@gmail.com>
2023-11-09 14:55:30 -0300

    cambio responsive

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.aparecer').style.opacity = 1;
+    }, 550); // 500 milisegundos = medio segundo
+});
